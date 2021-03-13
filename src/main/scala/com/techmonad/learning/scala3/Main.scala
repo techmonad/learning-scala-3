c949cdd4862657bb1895ec09e0da885b261a766c
Scala Steward <me@scala-steward.org>
2021-03-13 12:58:48 +0100

    Reformat with scalafmt 2.5.3

--- src/main/scala/com/techmonad/learning/scala3/Main.scala
+++ src/main/scala/com/techmonad/learning/scala3/Main.scala
@@ -4,9 +4,8 @@ import com.techmonad.learning.scala3.newtypes.IntersectionTypes
 
 object Main {
 
-  def main(args: Array[String]): Unit = {
+  def main(args: Array[String]): Unit =
     runExample("Intersection Types")(IntersectionTypes.test)
-  }
 
   private def runExample(name: String)(f: => Unit) = {
     println(Console.MAGENTA + s"$name examples: " + Console.RESET)
